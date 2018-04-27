d638adba88fe6327a8fd64584f8a780eeaae5295
lohitv <lohitvandanapu@gmail.com>
2018-04-27 15:45:35 -0400

    FFT for BSRM APRIL 27 2018

--- PhaseCoupling.py
+++ PhaseCoupling.py
@@ -9,29 +9,66 @@ from BSRM import *
 plt.style.use('seaborn')
 
 # # Generate a series of theta
-nsamples = 10000
+nsamples = 20000
 
-F = 50
-nf = 100
-df = F / nf
+# F = 50
+# nf = 100
+# df = F / nf
+#
+# T = 1 / df
+# nt = 200
+# dt = T / nt
+#
+# f = np.linspace(0, F - df, nf)
+# t = np.linspace(0, T - dt, nt)
+#
+# P = (f == 10) * 0.25 / df + (f == 20) * 0.25 / df + (f == 30) * 0.25 / df + (f == 40) * 0.25 / df
+# fx = f
+# fy = f
+# Fx, Fy = np.meshgrid(f, f)
+# B = 1 / np.sqrt(2) * ((Fx == 20) * (Fy == 20) * 0.25 / df ** 2 * 0.999999 + (Fx == 10) * (
+#         Fy == 30) * 0.125 / df ** 2 * 0.999999 + (Fx == 30) * (
+#                               Fy == 10) * 0.125 / df ** 2 * 0.999999)
+#
+# B_Real = B
+# B_Imag = B
+# B_Complex = B_Real + 1j * B_Imag
 
-T = 1 / df
-nt = 200
+# Input parameters
+T = 100  # Time(1 / T = dw)
+nt = 256  # Num.of Discretized Time
+F = 1 / T * nt / 2  # Frequency.(Hz)
+nf = 128 # Num of Discretized Freq.
+
+# # Generation of Input Data(Stationary)
 dt = T / nt
-
-f = np.linspace(0, F - df, nf)
 t = np.linspace(0, T - dt, nt)
+df = F / nf
+f = np.linspace(0, F - df, nf)
 
-P = (f == 10) * 0.25 / df + (f == 20) * 0.25 / df + (f == 30) * 0.25 / df + (f == 40) * 0.25 / df
+# Target PSDF(stationary)
+P = 20 * 1 / np.sqrt(2 * np.pi) * np.exp(-1 / 2 * f ** 2)
+
+t_u = 2 * np.pi / (2 * 2 * np.pi * F)
+if dt * 0.99 > t_u:
+    print('\n')
+    print('ERROR:: Condition of delta_t <= 2*np.pi/(2*2*np.pi*F_u) = 1/(2*W_u)')
+    print('\n')
+
+# Generating the 2 dimensional mesh grid
 fx = f
 fy = f
 Fx, Fy = np.meshgrid(f, f)
-B = 1 / np.sqrt(2) * ((Fx == 20) * (Fy == 20) * 0.25 / df ** 2 * 0.999999 + (Fx == 10) * (
-        Fy == 30) * 0.125 / df ** 2 * 0.999999 + (Fx == 30) * (
-                              Fy == 10) * 0.125 / df ** 2 * 0.999999)
 
-B_Real = B
-B_Imag = B
+b = 40 * 2 * 1 / (2 * np.pi) * np.exp(2 * (-1 / 2 * (Fx ** 2 + Fy ** 2)))
+B_Real = b
+B_Imag = b
+
+B_Real[0, :] = 0
+B_Real[:, 0] = 0
+B_Imag[0, :] = 0
+B_Imag[:, 0] = 0
+
 B_Complex = B_Real + 1j * B_Imag
 B_Ampl = np.absolute(B_Complex)
 Biphase = np.arctan2(B_Imag, B_Real)
@@ -63,12 +100,25 @@ for i in range(nf):
 
 ########################################################################################################################
 # BSRM Case
+import time
 
-obj = BSRM(nsamples, P, B_Complex, dt, df, nt, nf)
-samples = obj.samples
+time1 = time.time()
+P1 = np.copy(P)
+P1[0] = P1[0]/2
+obj = BSRM(nsamples, P1, B_Complex, dt, df, nt, nf)
+samples_BSRM = obj.samples
+print('Time to run BSRM is', time.time() - time1)
+
 Phi = obj.phi
 
-Xw = np.fft.fft(samples, axis=1)
+P_BSRM = estimate_PSD(samples_BSRM, nt, T)[1]
+
+plt.figure()
+plt.plot(P_BSRM)
+plt.plot(P)
+plt.show()
+
+Xw = np.fft.fft(samples_BSRM, axis=1)
 Xw = Xw[:, :nf]
 
 # Bispectrum
@@ -85,85 +135,43 @@ m_B[:, 0] = 0
 m_B_Ampl = np.absolute(m_B)
 m_B_Real = np.real(m_B)
 m_B_Imag = np.imag(m_B)
-
-# Plotting the Estimated Real Bispectrum function
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.plot_surface(Fx, Fy, m_B_Real)
-plt.title('Estimated $\Re{B(\omega_1, \omega_2)}$ - BSRM Case')
-ax.set_xlabel('$\omega_1$')
-ax.set_ylabel('$\omega_2$')
-plt.show()
-
-print('Results for BSRM case')
-print(B_Real[40, 40] / m_B_Real[40, 40])
-print(B_Real[20, 60] / m_B_Real[20, 60])
-
-B1 = np.fft.ifft(samples)
-Phi_re = np.imag(np.log(B1[:, :nf] / 2 / np.sqrt(df * P)))
 
 ########################################################################################################################
+# Reconstructing the phase angles
 
-theta_1 = Phi[:, 0]
+B1 = np.fft.ifft(samples_BSRM)
+B2 = B1[:, :nf]
+B2[:, 0] = B2[:, 0] + B1[:, 128]
+temp = np.conjugate(np.flip(B1[:, nf+1:], axis=1))
+B2[:, 1:] = B2[:, 1:] + np.conjugate(np.flip(B1[:, nf+1:], axis=1))
 
-theta_11 = np.exp((theta_1 + theta_1 + Biphase[0, 0]) * 1.0j)
-theta_2 = np.imag(np.log(np.sqrt(1 - sum_Bc2[1]) * Phi[:, 1] + np.sqrt(Bc2[0, 0]) * theta_11))
+########################################################################################################################
+# BSRM with Fourier Transform
 
-theta_21 = np.exp((theta_2 + theta_1 + Biphase[1, 0]) * 1.0j)
-theta_3 = np.imag(np.log(np.sqrt(1 - sum_Bc2[2]) * Phi[:, 2] + np.sqrt(Bc2[1, 0]) * theta_21))
-
-# Mu_22 = Biphase[40, 40]
-# rho_22 = np.sqrt(0.8)
-# biphase_22 = stats.wrapcauchy.rvs(rho_22, size=nsamples) + Mu_22
-
-# Mu_31 = Biphase[60, 20]
-# rho_31 = np.sqrt(0.2)
-# biphase_31 = stats.wrapcauchy.rvs(rho_31, size=nsamples) + Mu_31
-
-# theta_22 = np.exp((theta_2 + theta_2 + Mu_22) * (1.0j))
-# theta_31 = np.exp((theta_3 + theta_1 + Mu_31) * (1.0j))
-
-# theta_4 = np.imag(np.log(np.sqrt(1 - sum_Bc2[80])*Phi[:,80] + rho_22*theta_22 + rho_31*theta_31))
-
-Coeff = np.sqrt(df * P)
+time2 = time.time()
+Coeff = 2 * np.sqrt(df * P)
 Coeff[0] = Coeff[0] / np.sqrt(2)
-Phi = 2 * np.pi * np.random.uniform(size=[nsamples, nf])
-Phi[:, 20] = theta_1
-Phi[:, 40] = theta_2
-Phi[:, 60] = theta_3
-Phi[:, 80] = theta_4
 
-B = 2 * np.exp(Phi * 1.0j) * Coeff
-F = np.fft.fftn(B, [nt])
-F = np.real(F)
+Phi_e = np.exp(Phi * 1.0j)
+Biphase_e = np.exp(Biphase * 1.0j)
+B = np.sqrt(1 - sum_Bc2) * Phi_e
+Bc = np.sqrt(Bc2)
 
-Xw = np.fft.fft(F, axis=1)
-Xw = Xw[:, :nf]
+for i in range(nf):
+    for j in range(1, int(np.ceil((i+1)/2))):
+        f1 = j
+        f2 = i - j
+        B[:, i] = B[:, i] + Bc[f1, f2] * Biphase_e[f1, f2] * Phi_e[:, f1] * Phi_e[:, f2]
 
-# Bispectrum
-s_B = np.zeros([nsamples, nf, nf])
-s_B = s_B + 1.0j * s_B
-for i1 in range(nf):
-    for i2 in range(nf - i1):
-        s_B[:, i1, i2] = s_B[:, i1, i2] + (Xw[:, i1] * Xw[:, i2] * np.conj(Xw[:, i1 + i2]) / nt ** 2 / (nt / T)) * T
-m_B = np.mean(s_B, axis=0)
-# # # Set zero on X & Y axis
-m_B[0, :] = 0
-m_B[:, 0] = 0
+B = B * Coeff
+B[np.isnan(B)] = 0
+samples_PC = np.fft.fftn(B, [nt])
+samples_PC = np.real(samples_PC)
+print('Time taken for BSRM with FFt is', time.time() - time2)
 
-m_B_Ampl = np.absolute(m_B)
-m_B_Real = np.real(m_B)
-m_B_Imag = np.imag(m_B)
+P_PC = estimate_PSD(samples_PC, nt, T)[1]
 
-# Plotting the Estimated Real Bispectrum function
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.plot_surface(Fx, Fy, m_B_Real)
-plt.title('Estimated $\Re{B(\omega_1, \omega_2)}$ - Phase Coupling Case')
-ax.set_xlabel('$\omega_1$')
-ax.set_ylabel('$\omega_2$')
+plt.figure()
+plt.plot(P_PC)
+plt.plot(P)
 plt.show()
-
-print('Results for the Phase Coupling case')
-print(B_Real[40, 40] / m_B_Real[40, 40])
-print(B_Real[20, 60] / m_B_Real[20, 60])
